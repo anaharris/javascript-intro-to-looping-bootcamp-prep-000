d32f07ee0e7c3e928b4229aadc5ef47f80e687ba
Ana Harris <vana1990@gmail.com>
2018-09-07 03:30:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -24,8 +24,8 @@ function doWhileLoop(array){
   i = i + 1;
   }
   do {
-  array.pop;
-}
-while (array.length > 0 && incrementVariable());
-}
+    array.pop;
+  }
+  while (array.length > 0 && incrementVariable());
+  }
 
